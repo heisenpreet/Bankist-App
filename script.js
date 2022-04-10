8b63d1c7fc2f7f73cd93c816928f3e3998bf2ae2
Harpreet Singh <harpreetsinghsodi152@gmail.com>
2022-04-10 16:54:28 +0530

    v1

---
+++ script.js
@@ -0,0 +1,260 @@
+'use strict';
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+const transferForm = document.querySelector('.form--transfer');
+const closeForm = document.querySelector('.form--close');
+const loanForm = document.querySelector('.form--loan');
+
+/////////////////////////////////////////////////
+//NEWTOPIC MOVEMENTS DISPLAY FUNCTION   1
+
+const display = function (movements, sort = false) {
+  containerMovements.innerHTML = '';
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach((mov, key) => {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = ` <div class="movements__row">
+    <div class="movements__type movements__type--${type}">${
+      key + 1
+    } ${type}</div>
+    <div class="movements__date">3 days ago</div>
+    <div class="movements__value">${mov}</div>
+  </div>
+ 
+`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+/////////////////////////////////////////////////
+//NEWTOPIC Display balance FUNCTION   2
+
+const balnce = function (account) {
+  account.balance = account.movements.reduce((total, mov) => {
+    return total + mov;
+  }, 0);
+
+  labelBalance.textContent = `${account.balance}$`;
+}; // labelBalance.textContent = bal.join('');
+
+/////////////////////////////////////////////////
+//NEWTOPIC SUMMERY FUNCTION   3()
+
+const summery = function (account) {
+  const income = account.movements
+    .filter(amount => amount > 0)
+    .reduce((total, amount) => {
+      return total + amount;
+    }, 0);
+  labelSumIn.textContent = `${income}$`;
+
+  const out = account.movements
+    .filter(amount => amount < 0)
+    .reduce((total, amount) => {
+      return total + amount;
+    }, 0);
+  labelSumOut.textContent = `${Math.abs(out)}$`;
+
+  const interestAmount = account.movements
+    .filter(amount => amount > 0)
+    .map(amount => (amount * 1.2) / 100)
+    .filter(instamt => instamt >= 1)
+    .reduce((interest, amount) => {
+      return interest + amount;
+    }, 0);
+
+  labelSumInterest.textContent = `${interestAmount}$`;
+};
+
+/////////////////////////////////////////////////
+//NEWTOPIC CREATING USERNAME FUNCTION   4()
+
+const createUsername = function (account) {
+  account.forEach(acc => {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(el => el[0])
+      .join('');
+  });
+};
+createUsername(accounts);
+
+const updateUI = function (account) {
+  //display movement
+  display(account.movements);
+
+  //display balance
+  balnce(account);
+
+  //display summery
+
+  summery(account);
+};
+
+/////////////////////////////////////////////////
+//NEWTOPIC LOGIN FUNCTION   5
+let currentAcc;
+
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  currentAcc = accounts.find(
+    account => account.username === inputLoginUsername.value
+  );
+
+  if (currentAcc?.pin === Number(inputLoginPin.value)) {
+    //1Update the UI
+    labelWelcome.textContent = `Welcome ${currentAcc.owner.split(' ')[0]}`;
+    containerApp.style.opacity = 1;
+
+    updateUI(currentAcc);
+  } else {
+    labelWelcome.textContent = `Wrong Username/Passowrd`;
+    containerApp.style.opacity = 0;
+    //Update Ui with a erroe msg
+  }
+});
+
+/////////////////////////////////////////////////
+//NEWTOPIC MONEY TRANSFER FUNCTION   6
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const transferAmount = Number(inputTransferAmount.value);
+
+  const receiverAccount = accounts.find(
+    account => account.username === inputTransferTo.value
+  );
+
+  if (
+    receiverAccount &&
+    transferAmount > 0 &&
+    currentAcc.balance >= transferAmount &&
+    receiverAccount.username !== currentAcc.username
+  ) {
+    //doing the transfer
+    currentAcc.movements.push(-transferAmount);
+    receiverAccount.movements.push(transferAmount);
+    updateUI(currentAcc);
+  }
+
+  transferForm.reset();
+});
+
+/////////////////////////////////////////////////
+//NEWTOPIC ACCOUNT CLOSE FUNCTION   6
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    Number(inputClosePin.value) === currentAcc.pin &&
+    inputCloseUsername.value === currentAcc.username
+  ) {
+    const deleteIndex = accounts.findIndex(
+      acc => acc.username === currentAcc.username
+    );
+    accounts.splice(deleteIndex, 1);
+    containerApp.style.opacity = 0;
+  }
+
+  closeForm.reset();
+});
+
+/////////////////////////////////////////////////
+//NEWTOPIC LOAN  FUNCTION   7
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const loanAmount = Number(inputLoanAmount.value);
+
+  if (
+    loanAmount > 0 &&
+    currentAcc.movements.some(amount => amount >= loanAmount * 0.1)
+  ) {
+    currentAcc.movements.push(loanAmount);
+    updateUI(currentAcc);
+  }
+  loanForm.reset();
+});
+
+/////////////////////////////////////////////////
+//NEWTOPIC SORTING FUNCTION   8
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  display(currentAcc.movements, !sorted);
+  sorted = !sorted;
+});
+
+/////////////////////////////////////////////////
+// LECTURES
+
+/////////////////////////////////////////////////
